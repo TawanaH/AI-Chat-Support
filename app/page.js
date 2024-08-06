8ac333d1d1581174f7ca5d0f41a22af918dfc2a0
TawanaH <114176732+TawanaH@users.noreply.github.com>
2024-08-06 18:35:55 -0400

    update: base project complete

--- app/page.js
+++ app/page.js
@@ -11,24 +11,68 @@ export default function Home() {
     },
   ]);
 
+  const sendMessage = async () => {
+    setMessage("");
+    setMessages((messages) => [
+      ...messages,
+      { role: "user", content: message },
+      { role: 'assistant', content: ''}
+    ]);
+    const response = fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify([...messages, { role: "user", content: message }]),
+    }).then(async (res) => {
+      const reader = res.body.getReader()
+      const decoder = new TextDecoder()
+      let result = ''
+      return reader.read().then(function processText({done, value}){
+        if (done) {
+          return result
+        }
+        const text = decoder.decode(value || new Uint8Array(), {stream: true})
+        setMessages((messages) => {
+          let lastMessage = messages[messages.length - 1]
+          let otherMessages = messages.slice(0, messages.length - 1)
+
+          return [...otherMessages, {...lastMessage, content: lastMessage.content + text}]
+        })
+
+        return reader.read().then(processText)
+      })
+    });
+
+  };
+
+  const [message, setMessage] = useState("");
+
   return (
     <Box
       width="100vw"
       height="100vh"
       display="flex"
       flexDirection="column"
-      justiftContent="center"
+      justifyContent="center"
       alignItems="center"
+      p={10}
     >
       <Stack
-        direction={"column"}
+        direction={'column'}
         width="500px"
-        height="700px"
-        spacing={2}
+        maxHeight="700px"
         border="1px solid black"
         p={2}
+        spacing={3}
       >
-        <Stack direction={"column"} spacing={2} flexGrow={1}>
+        <Stack
+          direction={'column'}
+          spacing={2}
+          flexGrow={1}
+          overflow="auto"
+          maxHeight="100%"
+        >
           {messages.map((message, index) => (
             <Box
               key={index}
@@ -45,7 +89,7 @@ export default function Home() {
                 }
                 color={"white"}
                 borderRadius={16}
-                p={2}
+                p={3}
               >
                 {message.content}
               </Box>
@@ -53,8 +97,15 @@ export default function Home() {
           ))}
         </Stack>
         <Stack direction={"row"} spacing={2}>
-          <TextField label="Message" fullWidth />
-          <Button variant="contained">Send</Button>
+          <TextField
+            label="Message"
+            fullWidth
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button variant="contained" onClick={sendMessage}>
+            Send
+          </Button>
         </Stack>
       </Stack>
     </Box>
